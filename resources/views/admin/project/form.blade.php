8897676fbad6cd7bce3befe218b34b0a2126121d
mohamed <mohammed2571994@gmail.com>
2021-08-12 21:03:52 +0300

    Fix create project issue

--- resources/views/admin/project/form.blade.php
+++ resources/views/admin/project/form.blade.php
@@ -22,7 +22,7 @@
 
 <div class="form-group">
   <label for="url">{{ __('  رابط المشروع ') }}</label>
-  <input type="text" id="url" name="title" value="{{ $project->url ?? ''}}"
+  <input type="text" id="url" name="title" value="{{ $project->url ?? ''}}" name="url"
       class="form-control @error('url') is-invalid @enderror">
   @error('url')
       <span class="invalid-feedback">
