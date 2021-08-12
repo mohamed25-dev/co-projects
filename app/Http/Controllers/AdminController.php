98b9287a55d6601aa207db5d1656d1054a32af7b
mohamed <mohammed2571994@gmail.com>
2021-08-12 18:59:18 +0300

    Fix admin index issue

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,10 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\NewsLetter;
 use App\Models\Customer;
 use App\Models\Project;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Bus;
-use Illuminate\Support\Facades\DB;
-
 class AdminController extends Controller
 {
     public function index ()
@@ -16,8 +12,6 @@ class AdminController extends Controller
         $numberOfProjects = Project::count();
         $numberOfSubscribers = Customer::count();
         $numberOfNewsletters = NewsLetter::count();
-
-        $newsletter = NewsLetter::latest()->first();
 
         return view('admin.index', compact(
             'numberOfProjects',
