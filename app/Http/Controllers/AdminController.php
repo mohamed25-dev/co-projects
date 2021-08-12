3343f93688c68c5595d56e342120c4a7a0781f6e
mohamed <mohammed2571994@gmail.com>
2021-08-12 19:14:03 +0300

    Fix admin index auth issue

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,11 @@ use App\Models\Customer;
 use App\Models\Project;
 class AdminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index ()
     {
         $numberOfProjects = Project::count();
