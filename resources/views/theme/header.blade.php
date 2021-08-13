49d0c6fedabbcddcbdafe2997961e98cad44af0b
mohamed <mohammed2571994@gmail.com>
2021-08-13 14:47:10 +0300

    Fix drop down menu and header direction issues

--- resources/views/theme/header.blade.php
+++ resources/views/theme/header.blade.php
@@ -1,35 +1,37 @@
 <!-- Topbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-  <div class="me-4 lead"><b>@yield('heading')</b></div>
 
-  <!-- Sidebar Toggle (Topbar) -->
-  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle me-3">
-    <i class="fa fa-bars"></i>
-  </button>
 
-  <!-- Topbar Navbar -->
-  <ul class="navbar-nav me-auto">
-    
-    <div class="topbar-divider d-none d-sm-block"></div>
+    <!-- Topbar Navbar -->
+    <ul class="mx-2 navbar-nav container-fluid">
+        <div class="px-2 lead"><b>@yield('heading')</b></div>
 
-    
+        <!-- Sidebar Toggle (Topbar) -->
+        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle ">
+            <i class="fa fa-bars"></i>
+        </button>
 
-    <!-- Nav Item - User Information -->
-    <li class="nav-item dropdown no-arrow">
-      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-        aria-haspopup="true" aria-expanded="false">
-        <span class="me-2 d-none d-lg-inline text-gray-600 small">{{auth()->user()->name}}</span>
-      </a>
-      <!-- Dropdown - User Information -->
-      <div class="dropdown-menu dropdown-menu-left shadow animated--grow-in" style="right:-90px">
-        <a class="dropdown-item text-right" href="#" data-toggle="modal" data-target="#logoutModal">
-          <i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>
-          {{__('Logout')}}
-        </a>
-      </div>
-    </li>
+        <!-- Nav Item - User Information -->
+        <li class="nav-item dropdown no-arrow px-2">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <span class="me-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
+            </a>
+            <!-- Dropdown - User Information -->
+            <div class="dropdown-menu  shadow animated--grow-in {{$dir == 'rtl' ? 'text-right' : ''}}">
+                <a class="dropdown-item py-2" href="{{ route('profile.show') }}">
+                    <i class="fas fa-user fa-sm fa-fw text-gray-400"></i>
+                    {{ __('Profile') }}
+                </a>
 
-  </ul>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw text-gray-400"></i>
+                    {{ __('Logout') }}
+                </a>
+            </div>
+        </li>
+
+    </ul>
 
 </nav>
 <!-- End of Topbar -->
